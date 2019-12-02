746d5dba7de407472562174f057dc13fcde3bf37
Spladed <1310140596@qq.com>
2019-12-02 12:09:31 +0800

    修改了chargestaffdisplay的主xianshijiemian

--- src/chargeStaffDisplay/myLabel.java
+++ src/chargeStaffDisplay/myLabel.java
@@ -1,15 +1,24 @@
 package chargeStaffDisplay;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 
 public class myLabel {
-	public static JLabel Label(String str) {
+	public static JLabel borderLabel(String str) {
 		JLabel label=new JLabel(str,SwingConstants.CENTER);
-		Dimension preferredSize = new Dimension(50,20);
-    	label.setPreferredSize(preferredSize );
+		label.setSize(100, 50);
+		label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		return label;
+	}
+	
+	public static JLabel normalLabel(String str) {
+		JLabel label=new JLabel(str,SwingConstants.CENTER);
+		label.setSize(100, 50);
 		return label;
 	}
 }
